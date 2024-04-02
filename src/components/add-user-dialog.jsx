746ba72ce01ad7dfcbe7b4221bbbae568e0b80da
Elwyn <elwyn_de_neve@hotmail.com>
2024-04-02 09:30:27 +0200

    workshop starter

--- src/components/add-user-dialog.jsx
+++ src/components/add-user-dialog.jsx
@@ -11,7 +11,10 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { UserPlus } from "@phosphor-icons/react/dist/ssr";
+import {
+  Calendar as CalendarIcon,
+  UserPlus,
+} from "@phosphor-icons/react/dist/ssr";
 import {
   Select,
   SelectContent,
@@ -19,107 +22,119 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { DatePicker } from "@/components/date-picker";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { Calendar } from "@/components/ui/calendar";
 
 export function DialogButton() {
+  const today = new Date();
+  const [date, setDate] = React.useState(today);
+
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Button className="h-8 gap-1" size="sm">
-          <UserPlus className="h-3.5 w-3.5" />
-          <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-            Add User
-          </span>
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-md">
-        <DialogHeader>
-          <DialogTitle>Add User</DialogTitle>
-          <DialogDescription>
-            Add a new user to your database.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex items-center space-x-2 mt-2">
-          <div className="grid flex-1 gap-y-4 gap-4 items-start">
-            <div className="grid gap-2">
-              <Label className="text-sm" htmlFor="username">
-                Username
-              </Label>
-              <Input id="username" placeholder="" required />
-            </div>
-            <div className="flex flex-row gap-4">
-              <div className="grid flex-1 gap-2">
-                <Label className="text-sm" htmlFor="fistname">
-                  Firstname
+      <form action="">
+        <DialogTrigger asChild>
+          <Button className="h-8 gap-1" size="sm">
+            <UserPlus className="h-3.5 w-3.5" />
+            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+              Add User
+            </span>
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Add User</DialogTitle>
+            <DialogDescription>
+              Add a new user to your database.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex items-center space-x-2 mt-2">
+            <div className="grid flex-1 gap-y-4 gap-4 items-start">
+              <div className="grid gap-2">
+                <Label className="text-sm" htmlFor="username">
+                  Username
                 </Label>
-                <Input id="firstname" placeholder="" required />
+                <Input name="username" id="username" placeholder="" required />
               </div>
-              <div className="grid  flex-1 gap-2">
-                <Label className="text-sm" htmlFor="lastname">
-                  Lastname
-                </Label>
-                <Input id="genre" placeholder="" required />
+              <div className="flex flex-row gap-4">
+                <div className="grid flex-1 gap-2">
+                  <Label className="text-sm" htmlFor="fistName">
+                    First name
+                  </Label>
+                  <Input name="fistName" id="fistName" required />
+                </div>
+                <div className="grid  flex-1 gap-2">
+                  <Label className="text-sm" htmlFor="lastName">
+                    Last name
+                  </Label>
+                  <Input name="lastName" id="lastName" required />
+                </div>
               </div>
-            </div>
-            <div className="flex flex-row gap-4">
-              <div className="grid flex-1 gap-2">
-                <Label className="text-sm" htmlFor="status">
-                  Role
-                </Label>
-                <Select>
-                  <SelectTrigger className="">
-                    <SelectValue placeholder="Role" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="user">User</SelectItem>
-                    <SelectItem value="editor">Editor</SelectItem>
-                    <SelectItem value="admin">Admin</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className="grid  flex-1 gap-2">
-                <Label className="text-sm" htmlFor="date-picker">
-                  Registration Date
-                </Label>
-                <DatePicker />
+              <div className="flex flex-row gap-4">
+                <div className="grid flex-1 gap-2">
+                  <Label className="text-sm" htmlFor="status">
+                    Role
+                  </Label>
+                  <Select>
+                    <SelectTrigger className="">
+                      <SelectValue placeholder="Role" />
+                    </SelectTrigger>
+                    <SelectContent name="role">
+                      <SelectItem value="user">User</SelectItem>
+                      <SelectItem value="editor">Editor</SelectItem>
+                      <SelectItem value="admin">Admin</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="grid  flex-1 gap-2">
+                  <Label className="text-sm" htmlFor="date-picker">
+                    Registration Date
+                  </Label>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "justify-start text-left font-normal",
+                          !date && "text-muted-foreground",
+                        )}
+                      >
+                        <CalendarIcon className="mr-2 h-4 w-4" />
+                        {date ? format(date, "PPP") : <span>Pick a date</span>}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0">
+                      <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <DialogFooter className="sm:justify-end mt-4">
-          <DialogClose asChild>
-            <Button type="button" variant="secondary">
-              Cancel
-            </Button>
-          </DialogClose>
-          <DialogClose asChild>
-            <Button type="button" variant="default">
-              Add User
-            </Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
+          <DialogFooter className="sm:justify-end mt-4">
+            <DialogClose asChild>
+              <Button type="button" variant="secondary">
+                Cancel
+              </Button>
+            </DialogClose>
+            <DialogClose asChild>
+              <Button type="button" variant="default">
+                Add User
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </form>
     </Dialog>
-  );
-}
-
-function PlusCircleIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="10" />
-      <path d="M8 12h8" />
-      <path d="M12 8v8" />
-    </svg>
   );
 }
